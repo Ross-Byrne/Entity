9e19677abaaec6e9da85a952f054d00a1e5be958
Ross Byrne <ross.byrne@outlook.ie>
2017-08-06 20:41:30 +0100

    setting up the world graph to make A* searches possible

--- src/Generation/Node.java
+++ src/Generation/Node.java
@@ -7,16 +7,16 @@ import java.util.*;
  */
 public class Node {
 
-    private String name;
+    private int id;
     private int xPos;
     private int yPos;
 
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getxPos() {
@@ -34,5 +34,42 @@ public class Node {
     public void setyPos(int yPos) {
         this.yPos = yPos;
     }
+
+    // get an array of nodes that are adjacent to this node in the world
+    public Node[] adjacentNodes(Node[][] world){
+
+        java.util.List<Node> adjacents = new java.util.ArrayList<Node>();
+
+        if (yPos > 0) {
+           // if(maze[xPos - 1][yPos].getId() != 0) // only add if not a hedge
+                adjacents.add(world[yPos - 1][xPos]); //Add North
+        }
+        if (yPos < world.length - 1) {
+            //if(maze[yPos + 1][xPos].getId() != 0) // only add if not a hedge
+                adjacents.add(world[yPos + 1][xPos]); //Add South
+        }
+        if (xPos > 0) {
+            //if(maze[yPos][xPos - 1].getId() != 0) // only add if not a hedge
+                adjacents.add(world[yPos][xPos - 1]); //Add West
+        }
+        if (xPos < world[yPos].length - 1) {
+            //if(maze[yPos][xPos + 1].getId() != 0) // only add if not a hedge
+                adjacents.add(world[yPos][xPos + 1]); //Add East
+        }
+
+        return (Node[]) adjacents.toArray(new Node[adjacents.size()]);
+    }
+
+    // calculate the distance between this node and Node n
+    public double getDistance(Node n){
+
+        double x1 = this.xPos;
+        double y1 = this.yPos;
+        double x2 = n.getxPos();
+        double y2 = n.getyPos();
+
+        return (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+    } // calculateNodeDistance()
 
 } // class
